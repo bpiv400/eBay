6f771f393ddb6cdc84ec07a81318cab4d6adcf4a
Barry Plunkett <eplu@sas.upenn.edu>
2018-06-06 17:27:14 -0400

    added python template and processing files

---
+++ bash/py_temp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -N JOBNAME
+#$ -j y
+#$ -l m_mem_free=12G
+#$ -m e -M 4102158912@vtext.com
+
+scriptPath = PATHHERE
+cd ~
+source /opt/rh/rh-python36/enable
+source ~/envs/bargain/bin/activate
+cd eBay
+python $scriptPath
