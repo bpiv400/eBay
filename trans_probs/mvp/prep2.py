b1ddfd4bf1f470bbaa06494fd7059c83ffdfa68b
Barry Plunkett <eplu@sas.upenn.edu>
2018-06-27 17:58:29 -0400

    prep progress

--- trans_probs/mvp/prep2.py
+++ trans_probs/mvp/prep2.py
@@ -11,18 +11,26 @@ def main():
     # parse parameters
     parser = argparse.ArgumentParser(
         description='associate threads with all relevant variables')
+    # subdirectory corresponding to the type of file we're preprocessing
+    # (toy, train, test, etc.)
     parser.add_argument('--dir', action='store', type=str)
+    # name of the file we're pre-processing, should be
+    # 'subdir-n.csv'
     parser.add_argument('--name', action='store', type=str)
+    # turn name of the last offer made before the prediction variable
+    # for this data set
     parser.add_argument('--turn', action='store', type=str)
+    # name of the experiment
     parser.add_argument('--exp', action='store', type=str)
+    # parse arguments
     args = parser.parse_args()
-    print(args)
     filename = args.name
     subdir = args.dir
     low = args.low
     high = args.high
     step = args.step
     turn = args.turn.strip()
+    # verify turn argument is of the correct form
     if len(turn) != 2:
         raise ValueError('turn should be two 2 characters')
     turn_num = turn[1]
@@ -39,8 +47,15 @@ def main():
                      'auct_end_dt', 'item_price', 'bo_ck_yn'], inplace=True)
 
     date_list = []
+    # creating a list of date features that should be dropped
+    # in this case, we're dropping all date features encountered
     for i in range(turn_num + 1):
-
+        # up to and including the current turn number, drop all
+        # buyer date features (except time)
+        # and date for buyer and seller turns
+        # the date for all buyer and seller feautures is present
+        # up to and including the current turn because we include
+        # date for the prediction variable in extract_turns
         date_list.append('remain_' + 'b' + str(i))
         date_list.append('passed_' + 'b' + str(i))
         date_list.append('frac_remain_' + 'b' + str(i))
@@ -48,29 +63,59 @@ def main():
         date_list.append('date_b' + str(i))
         date_list.append('date_s' + str(i))
 
+        # up to but not including the current turn, if
+        # the final turn made is a buyer turn, remove all
+        # seller turn date features
+        # we exclude the current turn because we have yet
+        # to observe seller turn i on buyer turn i,
+        # so these features have been excluded from the data
         if i < turn_num and turn_type == 'b':
             date_list.append('remain_' + 's' + str(i))
             date_list.append('passed_' + 's' + str(i))
             date_list.append('frac_remain_' + 's' + str(i))
             date_list.append('frac_passed_' + 's' + str(i))
+        # if the current turn is a seller turn, remove all
+        # date features for sellers up to and including the
+        # current turn
         elif turn_type == 's':
             date_list.append('remain_' + 's' + str(i))
             date_list.append('passed_' + 's' + str(i))
             date_list.append('frac_remain_' + 's' + str(i))
             date_list.append('frac_passed_' + 's' + str(i))
-
+        # if we're not currently observing the first turn,
+        # remove the time associated with the buyer turn
         if i > 0:
             date_list.append('time_' + 'b' + str(i))
+        # remove the time associated with all seller turns
+        # up to and including the current turn (since
+        # we include time_si even when predicting
+        # time si
         date_list.append('time_s' + str(i))
 
-        if i == turn_num and turn_type == 'b':
-            if ('time_s' + str(i)) not in date_list:
-                date_list.append('time_s' + str(i))
-        elif i == turn_num and turn_type == 's':
+        ############################################################
+        # Deprecated --------- doesn't matter in this context where
+        # we remove all time variables any
+        ##########################################################
+        # turn we're currently observing the last turn number
+        # and the last turn we've observed is a buyer turn
+        #  if i == turn_num and turn_type == 'b':
+        #      # then remove the next seller time feature
+        #      if ('time_s' + str(i)) not in date_list:
+        #          date_list.append('time_s' + str(i))
+        ############################################################
+
+        # if we're currently observing the last turn
+        # and we're predicting a buyer turn (ie the last observed
+        # turn was as seller turn)
+        if i == turn_num and turn_type == 's':
+            # remove the date and time features for the next buyer turn
             date_list.append('time_b' + str(i+1))
             date_list.append('date_b' + str(i+1))
     # dropping all unused date and time features
     df.drop(columns=date_list, inplace=True)
+
+    # visual check of correctness
+    print(df.columns)
 
     # fixing seller and buyer history
     # assumed that NAN slr and byr histories indicate having participated in
@@ -86,7 +131,7 @@ def main():
     del no_hist_slr
 
     # setting percent feedback for 'green' sellers to median feedback
-    # score
+    # score -- a reasonable prior to assume buyers have about sellers
     never_sold = df[np.isnan(df['fdbk_pstv_src'].values)].index
     scores = df['fdbk_pstv_src'].values
     scores = scores[~np.isnan(scores)]
@@ -124,7 +169,8 @@ def main():
     del not_listed
 
     # setting nans for mssg to 0 arbitrarily since only ~.001% of offers
-    # have 0 for message
+    # have 0 for message, suggesting the overwhelming number o f Nan's corresopnd to
+    # not having a message
     no_msg = df[np.isnan(df['any_mssg'].values)].index
     df.loc[no_msg, 'any_mssg'] = 0
     del no_msg
@@ -137,10 +183,6 @@ def main():
 
     # dropping all threads that do not have ref_price1
     df.drop(df[np.isnan(df['ref_price1'].values)].index, inplace=True)
-
-    # making thread id the index
-    df.set_index('unique_thread_id', inplace=True)
-    # drop rows with offers below low threshold or start price above high threshhold
 
     save_loc = 'data/' + 'exps' + \
         '/' + exp_name + '/' turn + '/' + filename
