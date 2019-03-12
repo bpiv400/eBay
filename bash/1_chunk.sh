b140d4e2c9f376079877a77577953e031bb9d178
Etan Green - OID/Faculty <etangr@hpcc-login1.wharton.upenn.edu>
2019-03-12 11:42:19 -0400

    changes to bash

--- bash/1_chunk.sh
+++ bash/1_chunk.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-#$ -l m_mem_free=50G
+#$ -l m_mem_free=150G
 #$ -N chunk
 #$ -j y
 #$ -o logs/$JOB_NAME-$JOB_ID.log
 
-scriptPath=repo/processing/1_chunk.py
-cd ~
-source /opt/rh/rh-python36/enable
-source ~/envs/bargain/bin/activate
-cd eBay
+scriptPath=./repo/processing/1_chunk.py
 python "$scriptPath"
