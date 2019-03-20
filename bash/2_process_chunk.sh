7241f14d8d7af9a94ff8aa78c8f23e6a595c0b8f
Etan Green <etangreen@vpn-users-016-013.wharton.private.upenn.edu>
2019-03-20 17:23:05 -0400

    updating bash v1

--- bash/2_process_chunk.sh
+++ bash/2_process_chunk.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #$ -t 1-256
+#$ -q short.q
 #$ -N process_chunk
 #$ -j y
-#$ -o logs/$JOB_NAME-$JOB_ID-$SGE_TASK_ID.log
+#$ -o logs/
 
-scriptPath=./repo/processing/2_process_chunk.py
+scriptPath=repo/processing/2_process_chunk.py
 python "$scriptPath" --num "$SGE_TASK_ID"
