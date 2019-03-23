d2a1ec9a640a16248ff82db351ab407a8187f210
Etan Green <etangreen@huntsman-ve502-0934.apn.wlan.upenn.edu>
2019-03-23 13:16:59 -0400

    lda v4

--- lda/train.py
+++ lda/train.py
@@ -5,17 +5,17 @@ from gensim import models as lda
 DIR = './data/lda/'
 
 if __name__ == "__main__":
-    # number of topics
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--topics', action='store', type=int, required=True)
-    topics = parser.parse_args().topics
+	# number of topics
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--topics', action='store', type=int, required=True)
+	topics = parser.parse_args().topics
 
-    # input
-    tfidf = pickle.load(open(DIR + 'tfidf.pkl', 'rb'))
+	# input
+	tfidf = pickle.load(open(DIR + 'tfidf.pkl', 'rb'))
 
-    # train model
-    print('Training model')
-    model = lda.LdaModel(tfidf, num_topics=topics, eta='auto')
+	# train model
+	print('Training model')
+	model = lda.LdaModel(tfidf, num_topics=topics, eta='auto')
 
-    # save model
+	# save model
 	pickle.dump(model, open(DIR + 'm' + str(topics) + '.pkl', 'wb'))
