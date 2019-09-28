ea1c3d60d3a227b6dad6d101f67af5357f3ebeed
Etan Green <etangreen@vpn-users-016-026.wharton.private.upenn.edu>
2019-09-28 09:29:56 -0400

    limiting file size

--- processing/1_meta.py
+++ processing/1_meta.py
@@ -4,8 +4,10 @@ import pickle
 import pandas as pd, numpy as np
 from constants import *
 
+
 # read in data frames
 L = pd.read_csv(CLEAN_DIR + 'listings.csv').set_index('lstg')
+T = pd.read_csv(CLEAN_DIR + 'threads.csv').set_index(['lstg', 'thread'])
 O = pd.read_csv(CLEAN_DIR + 'offers.csv').set_index(
 	['lstg', 'thread','index'])
 
@@ -16,10 +18,11 @@ u = np.unique(L['meta'])
 for m in u:
     # extract associated listings and offers
     idx = L.loc[L['meta'] == m].index
-    L_i = L.loc[idx]
-    O_i = O.loc[idx]
+    L_i = L.loc[idx, ['meta', 'leaf', 'product', 'start_date', 'end_time']]
+    T_i = T.loc[idx, ['start_time']]
+    O_i = O.loc[idx, ['clock', 'price', 'accept', 'reject']]
 
     # write chunk
-    chunk = {'listings': L_i, 'offers': O_i}
+    chunk = {'listings': L_i, 'threads': T_i, 'offers': O_i}
     path = 'data/chunks/m%d.pkl' % m
     pickle.dump(chunk, open(path, 'wb'))
