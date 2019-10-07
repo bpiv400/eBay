da14c71b7fcce91d0da78e03ce945de57b8d12e8
Etan Green <etangreen@vpn-users-016-028.wharton.private.upenn.edu>
2019-10-07 15:07:20 -0400

    fixed days.sh

--- processing/5_inputs/days.sh
+++ processing/5_inputs/days.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#$ -t 1-3
 #$ -l m_mem_free=150G
 #$ -N days
 #$ -j y
 #$ -o logs/
 
-python repo/processing/5_inputs/days.py
+python repo/processing/5_inputs/days.py --num "$SGE_TASK_ID"
