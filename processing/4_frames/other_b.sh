d25ed0a67d86b934b1bf30053e847ccc18408132
Etan Green <etangreen@vpn-users-016-101.wharton.private.upenn.edu>
2019-10-07 11:39:05 -0400

    other_b.sh

--- processing/4_frames/other_b.sh
+++ processing/4_frames/other_b.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #$ -q all.q
 #$ -t 1-3
-#$ -l m_mem_free=50G
+#$ -l m_mem_free=150G
 #$ -N other_b
 #$ -j y
 #$ -o logs/
 
-python other_b.py --num "$SGE_TASK_ID"
+python repo/processing/4_frames/other_b.py --num "$SGE_TASK_ID"
