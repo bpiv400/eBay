6cb9ab6b85c06a5a899b99c2117bb17a4df12b5a
Barry Plunkett <bpiv400@gmail.com>
2019-10-28 12:05:03 -0400

    fixing task breakdown 2

--- processing/b_feats/meta_test.sh
+++ processing/b_feats/meta_test.sh
@@ -1,5 +1,6 @@
 for SGE_TASK_ID in {1..245}
 do
+  SGE_TASK_ID=$((SGE_TASK_ID - 1))
   NUM=$((SGE_TASK_ID / 7 + 1))
   FEAT=$((SGE_TASK_ID % 7 + 1))
   echo python repo/processing/b_feats/meta.py --num "$NUM" --feat "$FEAT"
