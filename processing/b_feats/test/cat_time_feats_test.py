fixed count normalization

processing/b_feats/test/cat_time_feats_test.py
@@ -574,56 +574,6 @@ def make_exp(exp, exp_array):
         exp = exp.sort_index()
     return exp
 
-
-def test_lstgs_open_meta():
-    events = setup_complex_lstg(None, meta=1, leaf=1)
-    events = setup_complex_lstg(events, meta=2, leaf=1, starter=11)
-    events.index = events.index.droplevel(['leaf', 'cndtn'])
-    # check values
-    exp_array = [0, 1, 2, 2, 0, 1, 2, 0, 1, 2, 1]
-    exp_array = [1 + exp for exp in exp_array]
-    exp = make_exp(None, exp_array)
-    exp = make_exp(exp, exp_array)
-
-    exp = exp['exp']
-    exp = exp - 1
-    actual = get_all_cat_feats(events, levels=['meta'])
-    assert np.all(np.isclose(exp.values, actual['meta_lstgs_open'].values))
-
-
-def test_lstgs_open_leaf():
-    events = setup_complex_lstg(None, meta=1, leaf=1)
-    events = setup_complex_lstg(events, meta=1, leaf=2, starter=11)
-    events.index = events.index.droplevel(['cndtn'])
-    actual = get_all_cat_feats(events, levels=['meta', 'leaf'])
-    exp_array = [0, 1, 2, 2, 0, 1, 2, 0, 1, 2, 1]
-    exp_array = [1 + exp for exp in exp_array]
-    exp = make_exp(None, exp_array)
-    exp = make_exp(exp, exp_array)
-    exp = exp['exp']
-    exp = exp - 1
-    assert np.all(np.isclose(exp.values, actual['leaf_lstgs_open'].values))
-    exp_array = [2, 4, 6, 6, 2, 4, 6, 2, 4, 6, 4]
-    exp = make_exp(None, exp_array)
-    exp = make_exp(exp, exp_array)
-    exp = exp['exp']
-    exp = exp - 1
-    assert np.all(np.isclose(exp.values, actual['meta_lstgs_open'].values))
-
-    events = setup_complex_lstg(None, meta=2, leaf=1)
-    events = setup_complex_lstg(events, meta=1, leaf=1, starter=11)
-    events.index = events.index.droplevel(['cndtn'])
-    actual = get_all_cat_feats(events, levels=['meta', 'leaf'])
-    exp_array = [0, 1, 2, 2, 0, 1, 2, 0, 1, 2, 1]
-    exp_array = [1 + exp for exp in exp_array]
-    exp = make_exp(None, exp_array)
-    exp = make_exp(exp, exp_array)
-    exp = exp['exp']
-    exp = exp - 1
-    assert np.all(np.isclose(exp.values, actual['leaf_lstgs_open'].values))
-    assert np.all(np.isclose(exp.values, actual['meta_lstgs_open'].values))
-
-
 def test_lstgs_meta():
     events = setup_complex_lstg(None, meta=1, leaf=1)
     events = setup_complex_lstg(events, meta=2, leaf=1, starter=11)
@@ -661,7 +611,7 @@ def test_slr_offers_meta():
     events.index = events.index.droplevel(['leaf', 'cndtn'])
     actual = get_all_cat_feats(events, levels=['meta'])
     exp_array = [6, 7, 8, 9, 9, 8, 9, 8, 8, 9, 9]
-    exp_array = [curr_exp / 11 for curr_exp in exp_array]
+    exp_array = [curr_exp / 10 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     print(exp.values)
@@ -675,7 +625,7 @@ def test_slr_offers_leaf():
     events.index = events.index.droplevel(['cndtn'])
     actual = get_all_cat_feats(events, levels=['meta', 'leaf'])
     exp_array = [6, 7, 8, 9, 9, 8, 9, 8, 8, 9, 9]
-    exp_array = [curr_exp / 11 for curr_exp in exp_array]
+    exp_array = [curr_exp / 10 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp.values, actual['leaf_slr_offers'].values))
@@ -683,7 +633,7 @@ def test_slr_offers_leaf():
     exp_array = [6, 7, 8, 9, 9, 8, 9, 8, 8, 9, 9]
     diff = [9 - ent for ent in exp_array]
     exp_array = [18 - ent_diff for ent_diff in diff]
-    exp_array = [curr_exp / 22 for curr_exp in exp_array]
+    exp_array = [curr_exp / 21 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp, actual['meta_slr_offers'].values))
@@ -693,7 +643,7 @@ def test_slr_offers_leaf():
     events.index = events.index.droplevel(['cndtn'])
     actual = get_all_cat_feats(events, levels=['meta', 'leaf'])
     exp_array = [6, 7, 8, 9, 9, 8, 9, 8, 8, 9, 9]
-    exp_array = [curr_exp / 11 for curr_exp in exp_array]
+    exp_array = [curr_exp / 10 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp.values, actual['leaf_slr_offers'].values))
@@ -707,7 +657,7 @@ def test_byr_offers_meta():
     actual = get_all_cat_feats(events, levels=['meta'])
     byr_offers = [5, 3, 2, 1, 2, 2, 1, 2, 1, 0, 0]
     exp_array = [sum(byr_offers) - curr for curr in byr_offers]
-    exp_array = [curr_exp / 11 for curr_exp in exp_array]
+    exp_array = [curr_exp / 10 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp.values, actual['meta_byr_offers'].values))
@@ -720,14 +670,14 @@ def test_byr_offers_leaf():
     actual = get_all_cat_feats(events, levels=['meta', 'leaf'])
     byr_offers = [5, 3, 2, 1, 2, 2, 1, 2, 1, 0, 0]
     exp_array = [sum(byr_offers) - curr for curr in byr_offers]
-    exp_array = [curr_exp / 11 for curr_exp in exp_array]
+    exp_array = [curr_exp / 10 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp.values, actual['leaf_byr_offers'].values))
 
     total = sum(byr_offers) * 2
     exp_array = [total - curr for curr in byr_offers]
-    exp_array = [curr_exp / 22 for curr_exp in exp_array]
+    exp_array = [curr_exp / 21 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp, actual['meta_byr_offers'].values))
@@ -738,7 +688,7 @@ def test_byr_offers_leaf():
     actual = get_all_cat_feats(events, levels=['meta', 'leaf'])
     byr_offers = [5, 3, 2, 1, 2, 2, 1, 2, 1, 0, 0]
     exp_array = [sum(byr_offers) - curr for curr in byr_offers]
-    exp_array = [curr_exp / 11 for curr_exp in exp_array]
+    exp_array = [curr_exp / 10 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp.values, actual['leaf_byr_offers'].values))
@@ -753,7 +703,7 @@ def test_threads_meta():
     threads = [3, 2, 1, 1, 2, 2, 1, 1, 1, 0, 0]
     total = sum(threads)
     exp_array = [total - curr for curr in threads]
-    exp_array = [curr_exp / 11 for curr_exp in exp_array]
+    exp_array = [curr_exp / 10 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp.values, actual['meta_threads'].values))
@@ -766,14 +716,14 @@ def test_threads_leaf():
     actual = get_all_cat_feats(events, levels=['meta', 'leaf'])
     threads = [3, 2, 1, 1, 2, 2, 1, 1, 1, 0, 0]
     exp_array = [sum(threads) - curr for curr in threads]
-    exp_array = [curr_exp / 11 for curr_exp in exp_array]
+    exp_array = [curr_exp / 10 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp.values, actual['leaf_threads'].values))
 
     total = sum(threads) * 2
     exp_array = [total - curr for curr in threads]
-    exp_array = [curr_exp / 22 for curr_exp in exp_array]
+    exp_array = [curr_exp / 21 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp, actual['meta_threads'].values))
@@ -784,7 +734,7 @@ def test_threads_leaf():
     actual = get_all_cat_feats(events, levels=['meta', 'leaf'])
     threads = [3, 2, 1, 1, 2, 2, 1, 1, 1, 0, 0]
     exp_array = [sum(threads) - curr for curr in threads]
-    exp_array = [curr_exp / 11 for curr_exp in exp_array]
+    exp_array = [curr_exp / 10 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp.values, actual['leaf_threads'].values))
@@ -799,7 +749,7 @@ def test_accepts_meta():
     accepts = [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]
     total = sum(accepts)
     exp_array = [total - curr for curr in accepts]
-    exp_array = [curr_exp / 11 for curr_exp in exp_array]
+    exp_array = [curr_exp / 10 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp.values, actual['meta_accepts'].values))
@@ -812,14 +762,14 @@ def test_accepts_leaf():
     actual = get_all_cat_feats(events, levels=['meta', 'leaf'])
     accepts = [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]
     exp_array = [sum(accepts) - curr for curr in accepts]
-    exp_array = [curr_exp / 11 for curr_exp in exp_array]
+    exp_array = [curr_exp / 10 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp.values, actual['leaf_accepts'].values))
 
     total = sum(accepts) * 2
     exp_array = [total - curr for curr in accepts]
-    exp_array = [curr_exp / 22 for curr_exp in exp_array]
+    exp_array = [curr_exp / 21 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp, actual['meta_accepts'].values))
@@ -830,7 +780,7 @@ def test_accepts_leaf():
     actual = get_all_cat_feats(events, levels=['meta', 'leaf'])
     accepts = [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]
     exp_array = [sum(accepts) - curr for curr in accepts]
-    exp_array = [curr_exp / 11 for curr_exp in exp_array]
+    exp_array = [curr_exp / 10 for curr_exp in exp_array]
     exp = make_exp(None, exp_array)
     exp = make_exp(exp, exp_array)['exp']
     assert np.all(np.isclose(exp.values, actual['leaf_accepts'].values))

processing/b_feats/util.py
@@ -342,8 +342,11 @@ def get_cat_lstg_counts(events, levels):
                             'byr_offer', 'accept']].groupby(by=curr_levels + ['lstg']).sum()
         ct_feats = ct_feats - ctl_feats
         per_lstg_feats = ['accept', 'slr_offer', 'byr_offer', 'thread']
+        divisor = ct_feats['lstg_ind'].copy()
+        divisor[divisor == 0] = 1
         for feat in per_lstg_feats:
-            ct_feats[feat] = ct_feats[feat] / (ct_feats['lstg_ind'] + 1)
+            ct_feats[feat] = ct_feats[feat] / divisor
+
         ct_feats = ct_feats.rename(lambda x: '_'.join([curr_levels[-1], x]) + 's', axis=1)
         tf = tf.join(ct_feats)
         tf.index = tf.index.droplevel(level=curr_levels)