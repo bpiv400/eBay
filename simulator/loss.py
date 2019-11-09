3bdc99f5f04aa2b6bb95b5ad9ad7d55b57a7aef8
Etan Green <etangreen@gmail.com>
2019-11-09 15:17:04 -0500

    removed printout from loss.py

--- simulator/loss.py
+++ simulator/loss.py
@@ -30,14 +30,6 @@ def cross_entropy_loss(theta, y):
 	# add in logistic log-likelihood for 4th byr turn
 	if ('theta4' in vars()) and (theta4.size()[0] > 0):
 		p4 = torch.sigmoid(theta4)	# probability of accept
-
-		print(theta4[0,:])
-		print(p4[0,:])
-		print(y4[0])
-		print(torch.log(p4[y4 == 100]))
-		print(torch.log(p4[y4 < 100]))
-		exit()
-
 		ll += torch.sum(torch.log(p4[y4 == 100])) \
 				+ torch.sum(torch.log(1-p4[y4 < 100]))
 
