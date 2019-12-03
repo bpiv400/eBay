7f48f4dbffa544ce8f6e4ebf17f540f35148a03e
Barry Plunkett <bpiv400@gmail.com>
2019-12-03 11:16:42 -0500

    updating branch with frozen simulator

--- simulator/model.py
+++ simulator/model.py
@@ -1,9 +1,8 @@
-import sys, math
-import torch, torch.optim as optim
+import sys
+import torch
 from torch.nn.utils import rnn
 from datetime import datetime as dt
 import numpy as np
-from simulator.loss import *
 from simulator.nets import *
 from constants import *
 
@@ -23,93 +22,95 @@ class Simulator:
         # save parameters from inputs
         self.model = model
         self.device = device
-        self.isRecurrent = model != 'hist'
-        self.c = params['c']
         self.mbsize = params['mbsize']
 
-        # size of theta and loss function
-        if model in ['hist', 'con_byr', 'con_slr']:
-            self.loss = cross_entropy_loss
+        # sloss function
+        if model in ['hist', 'con_slr']:
+            self.loss = torch.nn.CrossEntropyLoss(
+                reduction='sum')
+        elif model == 'con_byr':
+            self.loss = [torch.nn.CrossEntropyLoss(reduction='sum'),
+                         torch.nn.BCEWithLogitsLoss(reduction='sum')]
+        elif model == 'arrival':
+            self.loss = torch.nn.PoissonNLLLoss(
+                log_input=True, reduction='sum')
         else:
-            if model == 'arrival':
-                self.loss = poisson_loss
-            else:
-                self.loss = logit_loss
+            self.loss = torch.nn.BCEWithLogitsLoss(
+                reduction='sum')
 
         # neural net(s)
-        if not self.isRecurrent:
-            self.net = FeedForward(params, sizes).to(self.device)
-        elif ('delay' in model) or (model == 'arrival'):
+        if ('delay' in model) or (model == 'arrival'):
             self.net = LSTM(params, sizes).to(self.device)
         else:
-            self.net = RNN(params, sizes).to(self.device)          
+            self.net = FeedForward(params, sizes).to(self.device)     
 
-        # optimizer
-        self.optimizer = optim.Adam(self.net.parameters(), 
-            betas=(0.9, 1-math.pow(10, params['b2'])),
-            lr=math.pow(10, params['lr']))
 
     def evaluate_loss(self, d):
         # feed-forward
-        if not self.isRecurrent:
+        if 'x_time' not in d:
             y = d['y']
-            theta = self.net(d['x_fixed']).squeeze()
+            theta = self.net(d['x']).squeeze()
+
+            # for con_byr, split by turn and calculate loss separately
+            if self.model == 'con_byr':
+                # observation is on buyer's 4th turn if all three turn indicators are 0
+                t4 = torch.sum(d['x']['lstg'][:,-3:], dim=1) == 0
+
+                # loss for first 3 turns
+                loss = self.loss[0](theta[~t4,:], y[~t4].long())
+
+                # loss for last turn: use accept probability only
+                if torch.sum(t4).item() > 0:
+                    loss += self.loss[1](theta[t4,-1], (y[t4] == 100).float())
+
+                return loss
+            else:
+                if self.loss.__str__() == 'BCEWithLogitsLoss()':
+                    return self.loss(theta, y.float())
+                else:
+                    return self.loss(theta, y.long())
 
         # use mask for recurrent
-        else:
-            # retain indices with outcomes
-            mask = d['y'] > -1
+        mask = d['y'] > -1
 
-            # prediction from recurrent net
-            theta = self.net(d['x_fixed'], d['x_time'])
+        # prediction from recurrent net
+        theta = self.net(d['x'], d['x_time'])
 
-            # separate last turn of con_byr model
-            if self.model == 'con_byr':
-                # split y by turn
-                y = [d['y'][:,:3], d['y'][:,3]]
-
-                # apply mask separately
-                theta = [theta[0][mask[:,:3]], theta[1][mask[:,3]]]
-                y = [y[0][mask[:,:3]], y[1][mask[:,3]]]
-            else:
-                theta = theta[mask]
-                y = d['y'][mask]
-            
-        # calculate loss
+        # apply mask and calculate loss
+        theta = theta[mask]
+        y = d['y'][mask]
         return self.loss(theta, y)
 
-    def apply_max_norm_constraint(self, eps=1e-8):
-        for name, param in self.net.named_parameters():
-            if 'bias' not in name and 'output' not in name:
-                norm = param.norm(2, dim=1, keepdim=True)
-                desired = torch.clamp(norm, 0, self.c)
-                param = param * desired / (eps + norm)
 
-    def run_batch(self, d, isTraining):
+    def run_batch(self, d, optimizer=None):
         # train / eval mode
+        isTraining = optimizer is not None
         self.net.train(isTraining)
 
         # zero gradient
         if isTraining:
-            self.optimizer.zero_grad()
+            optimizer.zero_grad()
 
         # move to gpu
         if self.device != 'cpu':
-            d = {k: v.to(self.device) for k, v in d.items()}
+            for key, val in d.items():
+                if key == 'x':
+                    d[key] = {k: v.to(self.device) for k, v in val.items()}
+                else:
+                    d[key] = val.to(self.device)
 
-        if self.isRecurrent:
+        # pack x_time
+        if 'x_time' in d:
             d['x_time'] = rnn.pack_padded_sequence(
                 d['x_time'], d['turns'], batch_first=True)
 
         # calculate loss
         loss = self.evaluate_loss(d)
-        del d
 
         # step down gradients
         if isTraining:
             loss.backward()
-            self.optimizer.step()
-            self.apply_max_norm_constraint()
+            optimizer.step()
 
         # return log-likelihood
         return -loss.item()
--- simulator/nets.py
+++ simulator/nets.py
@@ -1,4 +1,4 @@
-import torch.nn as nn
+import torch, torch.nn as nn
 
 
 class FeedForward(nn.Module):
@@ -6,20 +6,33 @@ class FeedForward(nn.Module):
         # super constructor
         super(FeedForward, self).__init__()
 
+        # save dictionary of input feat sizes
+        self.k = sizes['x']
+
         # activation function
-        f = nn.ReLU()
-        #f = nn.Hardtanh(max_val=MAX)
+        self.f = nn.ReLU()
 
-        # initial layer
+        # embeddings layer(s)
+        d = {}
+        for k, v in self.k.items():
+            l = []
+            for i in range(2):
+                l += [nn.Linear(v, v), nn.BatchNorm1d(v), self.f]
+            d[k] = nn.ModuleList(l)
+        self.embedding = nn.ModuleDict(d)
+
+        # intermediate layer
         self.seq = nn.ModuleList(
-            [nn.Linear(sizes['fixed'], params['hidden']), f])
+            [nn.Linear(sum(self.k.values()), params['hidden']), 
+             nn.BatchNorm1d(params['hidden']), self.f])
 
-        # intermediate layers
+        # fully connected network
         for i in range(params['layers']-1):
             self.seq.append(nn.Dropout(p=params['dropout'] / 10))
             self.seq.append(
                 nn.Linear(params['hidden'], params['hidden']))
-            self.seq.append(f)
+            self.seq.append(nn.BatchNorm1d(params['hidden']))
+            self.seq.append(self.f)
 
         # output layer
         if toRNN:
@@ -28,70 +41,27 @@ class FeedForward(nn.Module):
             self.seq.append(nn.Linear(params['hidden'], sizes['out']))
 
     def forward(self, x):
-        for _, m in enumerate(self.seq):
+        '''
+        x: OrderedDict() with same keys as self.k
+        '''
+        # separate embeddings for input feature components
+        l = []
+        for key in self.k.keys():
+            x_k = x[key]
+            for m in self.embedding[key]:
+                x_k = m(x_k)
+            l.append(x_k)
+
+        # concatenate
+        x = torch.cat(l, dim=1)
+
+        # fully connected layers with dropout
+        for m in self.seq:
             x = m(x)
         return x
 
     def simulate(self, x):
         return self.forward(x)
-
-
-class RNN(nn.Module):
-    def __init__(self, params, sizes):
-
-        # super constructor
-        super(RNN, self).__init__()
-
-        # save parameters to self
-        self.layers = int(params['layers'])
-        self.steps = sizes['steps']
-
-        # initial hidden nodes
-        self.h0 = FeedForward(params, sizes, toRNN=True)
-
-        # rnn layer
-        self.rnn = nn.RNN(input_size=sizes['time'],
-                          hidden_size=int(params['hidden']),
-                          num_layers=self.layers,
-                          batch_first=True,
-                          dropout=params['dropout'] / 10)
-
-        # output layer
-        self.output = nn.Linear(params['hidden'], sizes['out'])
-
-        # for last byr turn
-        if self.steps == 4:
-            self.output4 = nn.Linear(params['hidden'], 1)
-
-    # output discrete weights and parameters of continuous components
-    def forward(self, x_fixed, x_time):
-        x_fixed = x_fixed.unsqueeze(dim=0).repeat(self.layers, 1, 1)
-        theta, _ = self.rnn(x_time, self.h0(x_fixed))
-
-        # pad
-        theta, _ = nn.utils.rnn.pad_packed_sequence(
-            theta, total_length=self.steps, batch_first=True)
-
-        # output layer: split if turns == 4 (i.e., con_byr model)
-        if self.steps == 4:
-            return self.output(theta[:,:self.steps-1,:]).squeeze(), \
-                self.output4(theta[:,self.steps-1,:]).squeeze()
-        else:
-            # (batch_size, seq_len, N_out)
-            return self.output(theta).squeeze()
-
-    def simulate(self, x_time, x_fixed=None, hidden=None, turn=0):
-        if hidden is None:
-            x_fixed = x_fixed.repeat(self.layers, 1, 1)
-            theta, hidden = self.rnn(x_time, self.h0(x_fixed))
-        else:
-            theta, hidden = self.rnn(x_time, hidden)
-        if turn == 7:
-            out = self.output4(theta)
-        else:
-            out = self.output(theta)
-        # output layer: (seq_len, batch_size, N_out)
-        return out, hidden
 
 
 class LSTM(nn.Module):
@@ -116,9 +86,10 @@ class LSTM(nn.Module):
         self.output = nn.Linear(params['hidden'], sizes['out'])
 
     # output discrete weights and parameters of continuous components
-    def forward(self, x_fixed, x_time):
-        x_fixed = x_fixed.unsqueeze(dim=0)
-        theta, _ = self.rnn(x_time, (self.h0(x_fixed), self.c0(x_fixed)))
+    def forward(self, x, x_time):
+        hidden = (self.h0(x).unsqueeze(dim=0), 
+            self.c0(x).unsqueeze(dim=0))
+        theta, _ = self.rnn(x_time, hidden)
 
         # pad
         theta, _ = nn.utils.rnn.pad_packed_sequence(
