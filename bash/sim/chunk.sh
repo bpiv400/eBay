0320c48162b580dbfc65d029c5bcb9a0fd9b6c29
Etan Green <etangreen@gmail.com>
2019-12-24 10:25:02 -0500

    env chunks

--- bash/sim/chunk.sh
+++ bash/sim/chunk.sh
@@ -8,10 +8,10 @@
 
 if [ "$SGE_TASK_ID" == 1 ]
 then
-	python repo/rlenv/simulator/chunk.py --part train_models
+	python repo/rlenv/simulator/chunk.py --part train_rl
 elif [ "$SGE_TASK_ID" == 2 ]
 then
-	python repo/rlenv/simulator/chunk.py --part train_rl
+	python repo/rlenv/simulator/chunk.py --part test_rl
 elif [ "$SGE_TASK_ID" == 3 ]
 then
 	python repo/rlenv/simulator/chunk.py --part test
