119d6cb0c4b7362df544bf787a7a1b9c6a0c0547
Barry Plunkett <bpiv400@gmail.com>
2020-01-14 11:50:15 -0500

    second script fix

--- bash/sync/sync_dropbox.sh
+++ bash/sync/sync_dropbox.sh
@@ -3,4 +3,4 @@ rclone sync ~/weka/eBay/inputs/sizes dropbox:ebay/data/inputs/sizes
 rclone copy ~/weka/eBay/inputs/params.pkl dropbox:ebay/data/inputs
 rclone copy ~/weka/eBay/inputs/date_feats.pkl dropbox:ebay/data/inputs
 rclone copy ~/weka/eBay/envSimulator/train_rl/chunks/1.gz dropbox:ebay/data/envSimulator/train_rl/chunks
-rclone sync ~/weka/eBay/outputs/models outputs/models --max-depth=1
+rclone sync ~/weka/eBay/outputs/models dropbox:ebay/data/outputs/models --max-depth=1
