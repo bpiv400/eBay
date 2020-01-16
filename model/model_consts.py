52a8dab4bf3e55209018be66c203f769d6738291
Etan Green <etangreen@gmail.com>
2020-01-15 21:55:22 -0500

    larger test mbsize

--- model/model_consts.py
+++ model/model_consts.py
@@ -5,7 +5,7 @@ LOG_DIR = OUTPUT_DIR + 'logs/'
 
 # optimization parameters
 NUM_WORKERS = 8
-MBSIZE = {True: 128, False: 128}
+MBSIZE = {True: 128, False: 2048}
 LOGLR0 = -3
 LOGLR1 = -6
 LOGLR_INC = 0.5
