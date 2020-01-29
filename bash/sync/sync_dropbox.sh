77cc20f062db37ab636f31d11a3c536ba54371ea
Barry Plunkett <bpiv400@gmail.com>
2020-01-29 12:51:58 -0500

    adding testing file to syncing scripts

--- bash/sync/sync_dropbox.sh
+++ bash/sync/sync_dropbox.sh
@@ -2,4 +2,5 @@ rclone sync ~/weka/eBay/inputs/featnames dropbox:ebay/data/inputs/featnames
 rclone sync ~/weka/eBay/inputs/sizes dropbox:ebay/data/inputs/sizes
 rclone copy ~/weka/eBay/inputs/date_feats.pkl dropbox:ebay/data/inputs
 rclone copy ~/weka/eBay/envSimulator/train_rl/chunks/1.gz dropbox:ebay/data/envSimulator/train_rl/chunks
+rclone copy ~/weka/eBay/envSimulator/train_rl/chunks/1_test.gz dropbox:ebay/data/envSimulator/train_rl/chunks
 rclone sync ~/weka/eBay/outputs/models dropbox:ebay/data/outputs/models --max-depth=1
