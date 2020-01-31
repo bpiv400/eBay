a38ce203529a14e6b6b7d8c715b17325b16a1d12
Barry Plunkett <bpiv400@gmail.com>
2020-01-31 11:57:01 -0500

    progress on building lstg log

--- rlenv/test/LstgLog.py
+++ rlenv/test/LstgLog.py
@@ -1,4 +1,8 @@
+import pandas as pd
+from featnames import START_TIME, MONTHS_SINCE_LSTG, BYR_HIST
 from rlenv.test.ThreadLog import ThreadLog
+from rlenv.env_consts import MODELS, ARRIVAL_MODEL
+from constants import MONTH
 
 class LstgLog:
 
@@ -7,20 +11,40 @@ class LstgLog:
         # TODO: Update
         :param params: A format decided upon by Etan and I in meeting
         """
-        self.params = params
-        self.arrivals = self.generate_arrival_logs()
-        self.threads = dict()
+        self.lstg = params['lstg']
+        self.lookup = params['lookup']
+        LstgLog.subset_params(params)
+        self.arrivals = self.generate_arrival_logs(params)
+        self.threads = self.generate_thread_logs(params)
         self.x_lstg = None # TODO: Create a dictionary containing all the x_lstg components that are common
         # among all models / turns (e.g. all but x_lstg)
-        for arrival in self.arrivals.keys():
-            self.threads[arrival] = self.generate_thread_log(thread=arrival)
 
-    def generate_arrival_logs(self):
-        print(self.params)
+    @property
+    def has_arrivals(self):
+        return len(self.arrivals) > 0
+
+    def generate_arrival_logs(self, params):
         arrival_logs = dict()
-        # TODO: Create an ArrivalLog for each arrival containing the necessary outcome data
-        # TODO: and Arrival Model / Hist Model inputs
+        if params['x_thread'] is None:
+            return arrival_logs
+        else:
+            for i in range(1, len(params['x_thread'].index) + 1):
+                curr_arrival = self.generate_arrival_log(params=params,
+                                                         thread_count=i)
+                arrival_logs[i] = curr_arrival
         return arrival_logs
+
+    def generate_arrival_log(self, params=None, thread_count=None):
+        time = params['x_thread'].loc[thread_count, MONTHS_SINCE_LSTG] * MONTH
+        time += self.lookup[START_TIME]
+        hist = params['x_thread'].loc[thread_count, BYR_HIST]
+        full_inputs = params['inputs'][ARRIVAL_MODEL]
+        inputs = dict()
+        for feat_set, feat_df in full_inputs.items():
+            curr_set = full_inputs[feat_set].loc[thread_count, :]
+            curr_set
+
+
 
     def generate_thread_log(self, thread=None):
         print(self.params)
@@ -36,5 +60,55 @@ class LstgLog:
         :return: np.float
         """
         con = self.threads[event.thread_id].get_con(event=event, x_lstg=self.x_lstg)
+
+    @staticmethod
+    def subset_params(params=None):
+        params['x_offer'] = LstgLog.subset_df(df=params['x_offer'],
+                                              lstg=params['lstg'])
+        params['x_thread'] = LstgLog.subset_df(df=params['x_thread'],
+                                              lstg=params['lstg'])
+        params['inputs'] = LstgLog.subset_inputs(input_data=params['inputs'],
+                                                 lstg=params['lstg'])
+
+    @staticmethod
+    def subset_df(df=None, lstg=None):
+        """
+        Subsets an arbitrary dataframe to only contain rows for the given
+        lstg
+        :param df: pd.DataFrame
+        :param lstg: integer giving lstg
+        :return: pd.Series or pd.DataFrame
+        """
+        if isinstance(df.index, pd.MultiIndex):
+            lstgs = df.index.unique(level='lstg')
+            if lstg in lstgs:
+                return df.xs(lstg, level='lstg', drop_level=True)
+        else:
+            if lstg in df.index:
+                return df.loc[lstg, :]
+        return None
+
+    @staticmethod
+    def subset_inputs(input_data=None, lstg=None):
+        inputs = dict()
+        for model in MODELS:
+            inputs[model] = dict()
+            index_is_cached = False
+            curr_index = None
+            for input_group, feats_df in input_data[model].items():
+                if not index_is_cached:
+                    if lstg in feats_df.index.unique(level='lstg'):
+                        full_lstgs = feats_df.index.get_level_values('lstg')
+                        curr_index = full_lstgs == lstg
+                    else:
+                        curr_index = None
+                    index_is_cached = True
+                if curr_index is None:
+                    subset = feats_df.loc[curr_index, :]
+                    subset.index = subset.index.droplevel(level='lstg')
+                else:
+                    subset = None
+                inputs[model][input_group] = subset
+        return input_data
 
 
--- rlenv/test/TestGenerator.py
+++ rlenv/test/TestGenerator.py
@@ -1,10 +1,8 @@
-import pandas as pd
 from compress_pickle import load
 from rlenv.simulator.Generator import Generator
 from rlenv.test.LstgLog import LstgLog
 from rlenv.test.TestEnvironment import TestEnvironment
 from rlenv.env_utils import get_env_sim_subdir
-from rlenv.env_consts import MODELS
 
 
 class TestGenerator(Generator):
@@ -28,11 +26,10 @@ class TestGenerator(Generator):
     def setup_env(self, lstg, lookup):
         params = {
             'lstg': lstg,
-            'inputs': self.subset_inputs(lstg),
-            'x_thread': TestGenerator.subset_df(df=self.test_data['x_thread'],
-                                                lstg=lstg),
-            'x_offer': TestGenerator.subset_df(df=self.test_data['x_offer'],
-                                               lstg=lstg)
+            'inputs': self.test_data['inputs'],
+            'x_thread': self.test_data['x_thread'],
+            'x_offer': self.test_data['x_offer'],
+            'lookup': lookup
         }
         self.log = LstgLog(params=params)
         return super().setup_env(lstg=lstg, lookup=lookup)
@@ -52,46 +49,6 @@ class TestGenerator(Generator):
                                arrival=self.arrival, x_lstg=x_lstg,
                                lookup=lookup, verbose=self.verbose,
                                log=self.log)
-
-    def subset_inputs(self, lstg):
-        inputs = dict()
-        input_data = self.test_data['inputs']
-        for model in MODELS:
-            inputs[model] = dict()
-            index_is_cached = False
-            curr_index = None
-            for input_group, feats_df in input_data[model].items():
-                if not index_is_cached:
-                    if lstg in feats_df.index.unique(level='lstg'):
-                        full_lstgs = feats_df.index.get_level_values('lstg')
-                        curr_index = full_lstgs == lstg
-                    else:
-                        curr_index = None
-                    index_is_cached = True
-                if curr_index is None:
-                    subset = feats_df.loc[curr_index, :]
-                else:
-                    subset = None
-                inputs[model][input_group] = subset
-        return input_data
-
-    @staticmethod
-    def subset_df(df=None, lstg=None):
-        """
-        Subsets an arbitrary dataframe to only contain rows for the given
-        lstg
-        :param df: pd.DataFrame
-        :param lstg: integer giving lstg
-        :return: pd.Series or pd.DataFrame
-        """
-        if isinstance(df.index, pd.MultiIndex):
-            lstgs = df.index.unique(level='lstg')
-            if lstg in lstgs:
-                return df.xs(lstg, level='lstg', drop_level=True)
-        else:
-            if lstg in df.index:
-                return df.loc[lstg, :]
-        return None
 
     @property
     def records_path(self):
