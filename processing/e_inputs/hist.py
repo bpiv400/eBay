d39232f648b49c4937f4c25f49d596596b4f9ac3
Etan Green <etangreen@gmail.com>
2020-02-06 22:45:45 -0500

    new hist feats

--- processing/e_inputs/hist.py
+++ processing/e_inputs/hist.py
@@ -1,25 +1,26 @@
-import numpy as np, pandas as pd
+import numpy as np
+import pandas as pd
 from processing.processing_utils import input_partition, load_file, save_files, init_x
-from featnames import CLOCK_FEATS, TIME_FEATS
+from featnames import CLOCK_FEATS, THREAD_COUNT
 
 
 # loads data and calls helper functions to construct train inputs
 def process_inputs(part):
 	# thread features
 	x_offer = load_file(part, 'x_offer').xs(1, level='index')
-	x_offer = x_offer[CLOCK_FEATS + TIME_FEATS]
-	x_thread = load_file(part, 'x_thread').join(x_offer)
+	x_thread = x_offer[CLOCK_FEATS].join(load_file(part, 'x_thread'))
+	x_thread[THREAD_COUNT] = x_thread.index.get_level_values(level='thread')
 
 	# outcome
 	y = x_thread['byr_hist']
 	idx = y.index
+	x_thread = x_thread.drop('byr_hist', axis=1)
 
 	# listing features
 	x = init_x(part, idx, drop_slr=True)
 
 	# add thread features to x['lstg']
-	x_thread = x_thread.drop('byr_hist', axis=1).astype('float32')
-	x['lstg'] = pd.concat([x['lstg'], x_thread], axis=1) 
+	x['lstg'] = pd.concat([x['lstg'], x_thread.astype('float32')], axis=1) 
 
 	return {'y': y, 'x': x}
 
