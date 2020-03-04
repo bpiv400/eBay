57a350c54f9d5334a2df06ae62930b1f1116a904
Etan Green <etangreen@gmail.com>
2020-03-04 16:49:18 -0500

    new bash files

--- bash/sim/values/values.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -t 1-10000
-#$ -q short.q
-#$ -l m_mem_free=8G
-#$ -N env_sim_vals
-#$ -j y
-#$ -o logs/
-
-python repo/rlenv/simulator/generate.py --num "$SGE_TASK_ID" --part "$1" --values
