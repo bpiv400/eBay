78f279ccc94ad9ade1568c81d9101482898e962c
Etan Green <etangreen@gmail.com>
2020-03-04 14:51:42 -0500

    one more bug fix

--- processing/processing_consts.py
+++ processing/processing_consts.py
@@ -109,5 +109,5 @@ NUM_OUT = {'first_arrival': INTERVAL_COUNTS[1] + 1,
            'msg4': 1,
            'msg5': 1,
            'msg6': 1,
-           'slr_init': CON_MULTIPLIER + 1,
-           'byr_init': CON_MULTIPLIER + 1}
+           'init_slr': CON_MULTIPLIER + 1,
+           'init_byr': CON_MULTIPLIER + 1}
