8c6470ef7407956d776b0327e889d7d93d9605ec
Etan Green <etangreen@gmail.com>
2020-03-04 14:47:37 -0500

    turn indicator names

--- featnames.py
+++ featnames.py
@@ -54,12 +54,8 @@ ALL_OFFER_FEATS = CLOCK_FEATS + TIME_FEATS + OUTCOME_FEATS
 
 # turn indices
 TURN_FEATS = {
-    'con_byr': ['t3', 't5'],
-    'con_slr': ['t2', 't4'],
-    'delay_byr': ['t3', 't5'],
-    'delay_slr': ['t2', 't4'],
-    'msg_byr': ['t3'],
-    'msg_slr': ['t2', 't4']
+    'init_byr': ['t1', 't3', 't5'],
+    'init_slr': ['t2', 't4']
 }
 
 # thread features
