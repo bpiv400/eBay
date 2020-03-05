9e68d344dfa0df03c31e9d6c754fa49c84c0a4d2
Etan Green <etangreen@gmail.com>
2020-03-05 14:17:58 -0500

    discrim.sh

---
+++ bash/train/discrim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -t 1-2
+#$ -N discrim
+#$ -o logs/train/
+#$ -j y
+
+if [ "$SGE_TASK_ID" == 1 ]
+then
+	python repo/train/train_model.py --name listings
+elif [ "$SGE_TASK_ID" == 2 ]
+then
+	python repo/train/train_model.py --name threads
+fi
