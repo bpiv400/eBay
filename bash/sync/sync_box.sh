8a494c1409511d832711627fc22eeb5b088ce447
bpiv400 <eplu@sas.upenn.edu>
2020-03-29 15:12:13 -0400

    updated sync scripts

--- bash/sync/sync_box.sh
+++ bash/sync/sync_box.sh
@@ -1,12 +1,12 @@
+# simulator inputs
 rclone sync dropbox:ebay/data/inputs/featnames inputs/featnames
 rclone sync dropbox:ebay/data/inputs/sizes inputs/sizes
 rclone copy dropbox:ebay/data/inputs/date_feats.pkl inputs
 rclone sync dropbox:ebay/data/outputs/models outputs/models --max-depth=1
 
-# grab specific chunks
-rclone copy dropbox:ebay/data/envSimulator/test_rl/chunks/1.gz envSimulator/test_rl/chunks
-rclone copy dropbox:ebay/data/envSimulator/test_rl/chunks/1_test.gz envSimulator/test_rl/chunks
+# grab all of test_rl
+rclone copy dropbox:ebay/data/envSimulator/test_rl/chunks envSimulator/test_rl/chunks
 
 # agent files
-rclone copy dropbox:ebay/data/agent/test_rl/input/slr.hdf5 agent/test_rl/input
+rclone copy dropbox:ebay/data/agent/train/seller.hdf5 agent/train
 
