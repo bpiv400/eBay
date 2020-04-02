8d6f5a7a5ed7c5fe8795dde2fa044c2d1aa875d2
bpiv400 <eplu@sas.upenn.edu>
2020-04-02 15:10:57 -0400

    logging fixed

--- agent/b_train/EbayRunner.py
+++ agent/b_train/EbayRunner.py
@@ -22,10 +22,13 @@ class EbayRunner(MinibatchRl):
         self.algo = None
         # initialize logging
         self._opt_infos = None
+        self._traj_infos = None
         self._cum_time = 0
         self._cum_completed_trajs = 0
+        self._new_completed_trajs = 0
         self._last_update_counter = 0
         self._last_time = 0
+        # super constructor
         super().__init__(algo=algo, agent=agent, sampler=sampler,
                          seed=seed, affinity=affinity,
                          n_steps=batch_size * batches_per_evaluation,
@@ -46,18 +49,19 @@ class EbayRunner(MinibatchRl):
         assert n_itr == self.batches_per_evaluation
         for itr in range(n_itr):
             logger.set_iteration(itr + self.itr_)
-            with logger.prefix(f"itr #{itr} "):
-                self.agent.sample_mode(itr)  # Might not be this agent sampling.
-                samples, traj_infos = self.sampler.obtain_samples(itr)
-                self.agent.train_mode(itr)
-                opt_info = self.algo.optimize_agent(itr, samples)
-                self.store_diagnostics(itr, traj_infos, opt_info)
+            with logger.prefix(f"itr #{itr + self.itr_} "):
+                self.agent.sample_mode(itr + self.itr_)  # Might not be this agent sampling.
+                samples, traj_infos = self.sampler.obtain_samples(itr + self.itr_)
+                self.agent.train_mode(itr + self.itr_)
+                opt_info = self.algo.optimize_agent(itr + self.itr_, samples)
+                self.store_diagnostics(itr + self.itr_, traj_infos, opt_info)
                 self.log_diagnostics(itr + self.itr_)
 
         self.itr_ += self.batches_per_evaluation
         self.shutdown()
 
     def initialize_logging(self):
+        self._traj_infos = deque(maxlen=self.log_traj_window)
         if self.itr_ == 0:
             self._opt_infos = {k: list() for k in self.algo.opt_info_fields}
         self._last_time = time.time()
@@ -82,6 +86,7 @@ class EbayRunner(MinibatchRl):
         # log diagnostics
         logger.record_tabular('CumTrainTime', self._cum_time)
         logger.record_tabular('Iteration', itr)
+        logger.record_tabular('NewCompletedTrajs', self._new_completed_trajs)
         logger.record_tabular('CumCompletedTrajs', self._cum_completed_trajs)
         logger.record_tabular('StepsPerSecond', samples_per_second)
         logger.record_tabular('UpdatesPerSecond', updates_per_second)
@@ -96,6 +101,8 @@ class EbayRunner(MinibatchRl):
         Store any diagnostic information from a training iteration that should
         be kept for the next logging iteration.
         """
+        self._new_completed_trajs += len(traj_infos)
+        self._traj_infos.extend(traj_infos)
         self._cum_completed_trajs += len(traj_infos)
         for k, v in self._opt_infos.items():
             new_v = getattr(opt_info, k, [])
