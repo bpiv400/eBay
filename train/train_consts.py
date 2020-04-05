933fa25b857fa4293f91312c7812f7cf179314ac
Etan Green <etangreen@gmail.com>
2020-04-05 14:43:24 -0400

    training not stopping bug fixed

--- train/train_consts.py
+++ train/train_consts.py
@@ -12,7 +12,7 @@ MBSIZE = {True: 128, False: 2048}  # True for training, False for validation
 # learning rate parameters
 LR_FACTOR = 0.1  # multiply learning rate by this factor when training slows
 LR0 = [1e-3]  # initial learning rates to search over
-LR1 = 1e-8  # stop training when learning rate is lower than this
+LR1 = 1e-7  # stop training when learning rate is lower than this
 FTOL = 1e-2  # decrease learning rate when relative improvement in loss is less than this
 
 # dropout grid
