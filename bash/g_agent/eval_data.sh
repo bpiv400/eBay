22003a6b976d538fbf5c56dcb084ce6f2f0e9a85
bpiv400 <eplu@sas.upenn.edu>
2020-04-06 18:14:48 -0400

    slight script change

--- bash/g_agent/eval_data.sh
+++ bash/g_agent/eval_data.sh
@@ -2,8 +2,8 @@
 #$ -t 1-1000
 #$ -q short.q
 #$ -l m_mem_free=4G
-#$ -N discrim
+#$ -N rl_eval_chunks
 #$ -j y
-#$ -o logs/discrim/
+#$ -o logs/
 
 python repo/agent/a_inputs/eval_chunks.py --num "$SGE_TASK_ID"
