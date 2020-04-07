51807b7fe20320df53ef04f86c9cb046418a93cb
bpiv400 <eplu@sas.upenn.edu>
2020-04-07 11:41:47 -0400

    changing syncing script

--- bash/sync/sync_box.sh
+++ bash/sync/sync_box.sh
@@ -5,7 +5,9 @@ rclone copy dropbox:ebay/data/inputs/date_feats.pkl inputs
 rclone sync dropbox:ebay/data/outputs/models outputs/models --max-depth=1
 
 # grab all of test_rl
-rclone copy dropbox:ebay/data/envSimulator/test_rl/chunks envSimulator/test_rl/chunks
+rclone copy dropbox:ebay/data/envSimulator/test_rl/chunks/1.gz envSimulator/test_rl/chunks
+# rclone copy dropbox:ebay/data/envSimulator/test_rl/chunks/1_test.gz envSimulator/test_rl/chunks
 
 # agent files
 rclone copy dropbox:ebay/data/agent/train/seller.hdf5 agent/train
+rclone sync dropbox:ebay/data/agent/eval agent/eval
--- bash/sync/sync_dropbox.sh
+++ bash/sync/sync_dropbox.sh
@@ -4,8 +4,11 @@ rclone sync ~/weka/eBay/inputs/sizes dropbox:ebay/data/inputs/sizes
 rclone copy ~/weka/eBay/inputs/date_feats.pkl dropbox:ebay/data/inputs
 rclone sync ~/weka/eBay/outputs/models dropbox:ebay/data/outputs/models --max-depth=1
 
-# grab all of test_rl
-rclone sync ~/weka/eBay/envSimulator/test_rl/chunks dropbox:ebay/data/envSimulator/test_rl/chunks
+# grab all of 1 chunk and the test data from test_rl
+rclone copy ~/weka/eBay/envSimulator/test_rl/chunks/1.gz dropbox:ebay/data/envSimulator/test_rl/chunks
+# rclone copy ~/weka/eBay/envSimulator/test_rl/chunks/1_test.gz dropbox:ebay/data/envSimulator/test_rl/chunks
+
 
 # agent training input files
 rclone copy ~/weka/eBay/agent/train/seller.hdf5 dropbox:ebay/data/agent/train
+rclone sync ~/weka/eBay/agent/eval dropbox:ebay/data/agent/eval
