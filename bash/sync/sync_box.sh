f88631f16ccb8314aa32160c07f9bf04df629588
bpiv400 <eplu@sas.upenn.edu>
2020-05-09 10:57:24 -0400

    script to fix old architecture problems

--- bash/sync/sync_box.sh
+++ bash/sync/sync_box.sh
@@ -5,7 +5,10 @@ rclone copy dropbox:ebay/data/inputs/date_feats.pkl inputs
 rclone sync dropbox:ebay/data/outputs/models outputs/models --max-depth=1
 
 # grab all of test_rl
-rclone copy dropbox:ebay/data/partitions/test_rl/chunks/1.gz partitions/test_rl/chunks
+for partition in test_rl train_rl
+do
+  rclone copy dropbox:ebay/data/partitions/${partition}/chunks/1.gz partitions/${partition}/chunks
+done
 rclone copy dropbox:ebay/data/partitions/test_rl/chunks/1_test.gz partitions/test_rl/chunks
 
 # agent files
---
+++ nets/update_state_dicts.py
@@ -0,0 +1,86 @@
+"""
+Updates the model state dictionaries from the old structure where
+there the output layer was the last layer of a set of sequential layers
+to the new structure where it's called output
+"""
+import argparse
+import os
+import torch
+from constants import MODELS, PREFIX
+from utils import load_state_dict, load_sizes
+from nets.nets_consts import LAYERS_FULL
+from nets.FeedForward import FeedForward
+BACKUP_DIR = '{}/backup/'.format(PREFIX)
+
+
+def update_dicts():
+    if not os.path.isdir(BACKUP_DIR):
+        os.mkdir(BACKUP_DIR)
+    for model in MODELS:
+        state_dict = load_state_dict(model)
+        backup_path = '{}{}.net'.format(BACKUP_DIR, model)
+        torch.save(state_dict, backup_path)
+        fully_connected_compat(state_dict=state_dict)
+        check_compat(model_name=model, state_dict=state_dict)
+
+
+def check_compat(model_name=None, state_dict=None):
+    sizes = load_sizes(model_name)
+    net = FeedForward(sizes)
+    try:
+        net.load_state_dict(state_dict, strict=True)
+        print('{} is compatible after update'.format(model_name))
+    except RuntimeError as err:
+        print('{} is not compatible after update'.format(model_name))
+        print(err)
+        exit(1)
+
+
+def fully_connected_compat(state_dict=None):
+    """
+    Renames the parameters in a torch state dict generated
+    when output layer lived in FullyConnected to be compatible
+    with separate output layer
+    :param state_dict: dictionary name -> param
+    :return: dict
+    """
+    old_prefix = 'nn1.seq.{}'.format(LAYERS_FULL)
+    new_prefix = 'output'
+    substitute_prefix(old_prefix=old_prefix, new_prefix=new_prefix,
+                      state_dict=state_dict)
+
+
+def substitute_prefix(old_prefix=None, new_prefix=None, state_dict=None):
+    effected_keys = list()
+    for key in state_dict.keys():
+        if key[:len(old_prefix)] == old_prefix:
+            effected_keys.append(key)
+    # null case
+    if len(effected_keys) == 0:
+        return state_dict
+
+    # replace each old prefix with a new prefix
+    for effected_key in effected_keys:
+        effected_suffix = effected_key[len(old_prefix):]
+        new_key = '{}{}'.format(new_prefix, effected_suffix)
+        state_dict[new_key] = state_dict[effected_key]
+        del state_dict[effected_key]
+
+
+def replace_dicts():
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--step', type=str, default=1)
+    step = parser.parse_args().step
+    if step == 1:
+        update_dicts()
+        print('Done storing backups')
+    else:
+        replace_dicts()
+
+
+if __name__ == '__main__':
+    main()
