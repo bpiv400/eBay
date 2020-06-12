57139b14c95d965499b9f9d77f136991d06bcca8
Barry Plunkett <bpiv400@gmail.com>
2020-06-12 12:21:48 -0400

    sync scripts updated

--- bash/sync/sync_box.sh
+++ bash/sync/sync_box.sh
@@ -9,6 +9,13 @@ for partition in test_rl train_rl
 do
   rclone copy dropbox:ebay/data/partitions/${partition}/chunks/1.gz partitions/${partition}/chunks
 done
+for init_type in policy value
+do
+    rclone copy dropbox:ebay/data/inputs/test_rl/${init_type}_slr.gz inputs/test_rl/
+    rclone copy dropbox:eBay/data/inputs/test_rl/${init_type}_slr_delay.gz inputs/test_rl
+    rclone copy dropbox:eBay/data/inputs/test_rl/${init_type}_byr_delay.gz inputs/test_rl
+done
+
 rclone copy dropbox:ebay/data/partitions/test_rl/chunks/1_test.gz partitions/test_rl/chunks
 
 # agent files
