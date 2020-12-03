d34412d199e516c1f5970cfeacc2440bbefc0fbc
Etan Green <etangreen@gmail.com>
2020-12-03 14:05:02 -0800

    bug fix

--- agent/eval/collate.py
+++ agent/eval/collate.py
@@ -19,7 +19,8 @@ def main():
         sims.append(unpickle(chunk_path))
 
     # clean and save
-    process_sims(part=args['part'], sims=sims, output_dir=output_dir, byr=args[BYR])
+    process_sims(part=args['part'], sims=sims, output_dir=output_dir,
+                 byr=args[BYR], save_inputs=(not args.heuristic))
 
 
 if __name__ == '__main__':
